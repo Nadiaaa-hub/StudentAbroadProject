c1a2edf468b1500fa15b7df4d885a7140831ecfd
KIB101D <152413382+KIB101D@users.noreply.github.com>
2025-08-01 14:50:03 +0200

    Update main.js
    - code autor Nadiaa-hub

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 import { initLanguageSwitcher } from "./modules/language-switcher.js";
 import { initScrollIndicator } from "./modules/scroll-indicator.js";
-import { setupSearchAndHighlight } from "./modules/highlighting.js";
-setupSearchAndHighlight();
-
 import { initCustomSelects } from "./modules/custom-select.js";
 import { initCountryFilter } from "./modules/country-filter.js";
 import { initAddUniversity } from "./modules/add-university.js";
+import { initSearchInput } from "./modules/search.js";
 import { initFAQ } from "./modules/faq.js";
+import { modalBtnClose } from "./modules/modal.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initLanguageSwitcher();
   initScrollIndicator();
-
 
   const searchInput = document.querySelector(".search-box__input");
   const searchableSections = document.querySelectorAll(
@@ -20,5 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   initCustomSelects();
   initCountryFilter();
   initAddUniversity();
+  initSearchInput();
+  modalBtnClose();
   initFAQ();
 });
