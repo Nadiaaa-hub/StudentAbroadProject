d2a5c3177ee1c4bbb23c9783c902beef66eda59c
Nadia <nadya2000006@gmail.com>
2025-09-09 22:26:59 +0300

    fix bugs in faq-page.

--- js/modules/scroll-indicator.js
+++ js/modules/scroll-indicator.js
@@ -9,7 +9,6 @@ export function initScrollIndicator() {
     });
   }
 
-  //підсвічування в навігації певної сторінки
   const currentPage = window.location.pathname.split("/").pop();
   const menuLinks = document.querySelectorAll(
     ".header__menu a, .footer__menu a"
@@ -21,5 +20,26 @@ export function initScrollIndicator() {
     if (linkPage === currentPage) {
       link.classList.add("active");
     }
+  });
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    });
+  });
+
+  document.querySelectorAll(".btn").forEach((btn) => {
+    btn.addEventListener("touchstart", () => {
+      btn.classList.add("hover");
+    });
+    btn.addEventListener("touchend", () => {
+      btn.classList.remove("hover");
+    });
   });
 }
