f1a84ad8e44d7bbb401dbcbcda84177873924e90
Nadiaaa-hub <nadya2000006@gmail.com>
2025-10-30 20:58:05 +0200

    Import and initialize initTrimText function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
+// main.js - додаємо імпорт та ініціалізацію
+
 import { initLanguageSwitcher } from "./modules/language-switcher.js";
 import { initScrollIndicator } from "./modules/scroll-indicator.js";
 import { initCustomSelects } from "./modules/custom-select.js";
 import { initCountryFilter } from "./modules/country-filter.js";
 import { initAddUniversity } from "./modules/add-university.js";
 import { initFAQ } from "./modules/faq.js";
+import { initSlider } from "./modules/slider-mobile.js";
 import { modalBtnClose } from "./modules/modal.js";
-import { initFormSubmit } from "./modules/form-submit.js";
 import { burgerMenu } from "./modules/burger-menu.js";
 import { initProgramSearch } from "./modules/program-search.js";
-import { initSlider } from "./modules/slider-mobile.js";
+import { initTrimText } from "./modules/trimProgramText.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initLanguageSwitcher();
   initScrollIndicator();
-  initFormSubmit();
   initCustomSelects();
   initCountryFilter();
   initAddUniversity();
@@ -22,4 +23,5 @@ document.addEventListener("DOMContentLoaded", () => {
   burgerMenu();
   initFAQ();
   initProgramSearch();
+  initTrimText()
 });
